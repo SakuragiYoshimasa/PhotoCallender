8e5e45f7bbf0fef5f81cc7242bf00e1b1a33dfce
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-06-22 19:28:04 +0900

    initial commit

---
+++ PhotoCallender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoCallender
+//
+//  Created by 櫻木善将 on 2015/06/22.
+//  Copyright © 2015年 YoshimasaSakuragi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
